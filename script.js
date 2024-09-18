c20ea7361043362c8a10dcfedb5777d3e7d4d4cf
Devansh Rai <devanshrai2003@gmail.com>
2024-09-18 23:28:04 +0530

    Initial files

---
+++ script.js
@@ -0,0 +1,26 @@
+const canvas = document.body;
+const menu = document.querySelector(".menu");
+const colorButtons = document.querySelectorAll(".color-btn");
+const addNewBtn = document.querySelector("#submit");
+
+colorButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const selectedColor = button.classList[1];
+        canvas.style.backgroundColor = selectedColor;
+    });
+});
+
+addNewBtn.addEventListener("click", () => {
+    const newBtn = document.createElement("button");
+    const colorPicker = document.getElementById("custom-color");
+    const newColor = colorPicker.value;
+
+    newBtn.classList.add("color-btn");
+    newBtn.style.backgroundColor = newColor;
+    newBtn.innerText = newColor
+    newBtn.addEventListener("click", () => {
+        canvas.style.backgroundColor = newColor;
+    })
+
+    menu.appendChild(newBtn);
+})
