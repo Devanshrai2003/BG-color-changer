135092ae6f98a541176f6d210f81569321af4dc3
Devansh Rai <devanshrai2003@gmail.com>
2025-06-02 15:32:34 +0530

    UI update

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
-const canvas = document.body;
+const canvas = document.querySelector(".canvas");
 const menu = document.querySelector(".menu");
 const buttons = document.querySelector(".buttons");
 const colorButtons = document.querySelectorAll(".color-btn");
-const addNewBtn = document.querySelector("#submit");
+const addNewBtn = document.querySelector("#add-btn");
 
-colorButtons.forEach(button => {
-    button.addEventListener("click", () => {
-        const selectedColor = button.classList[1];
-        canvas.style.backgroundColor = selectedColor;
-    });
+colorButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const selectedColor = button.classList[1];
+    canvas.style.backgroundColor = selectedColor;
+  });
 });
 
 addNewBtn.addEventListener("click", () => {
-    const newBtn = document.createElement("button");
-    const colorPicker = document.getElementById("custom-color");
-    const newColor = colorPicker.value;
+  const colorPicker = document.getElementById("custom-color");
+  const newColor = colorPicker.value;
 
-    newBtn.classList.add("color-btn");
-    newBtn.style.backgroundColor = newColor;
-    newBtn.innerText = newColor
-    newBtn.addEventListener("click", () => {
-        canvas.style.backgroundColor = newColor;
-    })
+  const newBtn = document.createElement("button");
 
-    buttons.appendChild(newBtn);
-})
+  newBtn.classList.add("color-btn");
+  newBtn.style.backgroundColor = newColor;
+  newBtn.innerText = newColor;
+  newBtn.addEventListener("click", () => {
+    canvas.style.backgroundColor = newColor;
+  });
+
+  buttons.appendChild(newBtn);
+});
